51b85422ae01d901a324181a88b9775de1188ea9
laki <2472592311@qq.com>
2022-07-07 16:15:16 +0800

    the three time

---
+++ yeb-server/src/main/java/com/laki/server/YebApplication.java
@@ -0,0 +1,17 @@
+package com.laki.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author laki
+ * @date 2022/7/7 16:01
+ */
+@SpringBootApplication
+public class YebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YebApplication.class,args);
+    }
+}
