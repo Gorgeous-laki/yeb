766ae5cf11c6ce21fbde7c48a14fa3f23ffc8534
laki <2472592311@qq.com>
2022-07-07 15:20:35 +0800

    the first time

---
+++ src/test/java/com/laki/yeb/YebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laki.yeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
